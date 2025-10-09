cb3ca81e8e320aceb9e3cefbf7ffdea79be866f4
Bardiya Basafa <Bardiya.Basafa@gmail.com>
2025-10-09 13:38:54 +0330

    pull req commit

--- src/Services/Notification.Service/Notification.Service/obj/Debug/net9.0/Notification.Service.AssemblyInfo.cs
+++ src/Services/Notification.Service/Notification.Service/obj/Debug/net9.0/Notification.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notification.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+452ee50c24097cea85a5d9ac7831615c2f183355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b39dd7559a547e3ceff55626db6fd5b2c66a99c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notification.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notification.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
