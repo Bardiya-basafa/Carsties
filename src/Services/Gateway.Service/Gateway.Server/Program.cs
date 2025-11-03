3794f7aa11b7e61892ec27981f5dbff790698d70
Bardiya Basafa <Bardiya.Basafa@gmail.com>
2025-11-03 23:37:51 +0330

    fixing cors policy of gateway to localhost:3000

--- src/Services/Gateway.Service/Gateway.Server/Program.cs
+++ src/Services/Gateway.Service/Gateway.Server/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowNextJS",
     policy => {
-        policy.WithOrigins("https://app.carsties.local")
+        policy.WithOrigins("http://localhost:3000")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
