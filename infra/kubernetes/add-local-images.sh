9504b874b74ff9abe2a2fa9d8177535f85359c2d
Bardiya Basafa <Bardiya.Basafa@gmail.com>
2025-11-14 16:37:01 +0330

    fixing script

--- infra/kubernetes/add-local-images.sh
+++ infra/kubernetes/add-local-images.sh
@@ -1,4 +1,19 @@
 # Rebuild, save, and deploy
+# Saving stage 
+docker save bardiyabasafa/carsties-auction-api -o ~/k8s/carsties-auction-api.tar && \
+docker save bardiyabasafa/carsties-identity-server -o ~/k8s/carsties-identity-server.tar && \
+docker save bardiyabasafa/carsties-search-api -o ~/k8s/carsties-search-api.tar && \
+docker save bardiyabasafa/carsties-bids-api -o ~/k8s/carsties-bids-api.tar && \ 
+docker save bardiyabasafa/carsties-notification-hub -o ~/k8s/carsties-notification-hub.tar && \ 
+docker save bardiyabasafa/carsties-gateway -o ~/k8s/carsties-gateway.tar && \ 
+docker save bardiyabasafa/carsties-webapp -o ~/k8s/carsties-webapp.tar && \ 
+docker save postgres -o ~/k8s/postgres.tar && \ 
+docker save redis -o ~/k8s/redis.tar && \ 
+docker save mongo -o ~/k8s/mongo.tar && \ 
+docker save rabbitmq -o ~/k8s/rabbitmq.tar && \ 
+docker save nginx:trixie-perl -o ~/k8s/nginx.tar && \ 
+
+# Importing images
 docker cp ~/k8s desktop-control-plane:/opt/ 
 docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-auction-api.tar && \ 
 docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-bids-api.tar && \ 
@@ -7,5 +22,8 @@ docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-
 docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-notification-hub.tar && \ 
 docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-gateway.tar && \ 
 docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-webapp.tar && \ 
-# kubectl rollout restart deployment/carsties-auction-api && \
-# rm carsties-api.tar
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/postgres.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/redis.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/mongo.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/rabbitmq.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/nginx.tar && \ 
