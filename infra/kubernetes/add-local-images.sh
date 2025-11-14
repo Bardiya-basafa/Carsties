0cdef955a1cd1afd9f7b25e1fb720fd10adb6dd2
Bardiya Basafa <Bardiya.Basafa@gmail.com>
2025-11-14 16:26:59 +0330

    adding script for loading localdocker image to docker cluster

--- infra/kubernetes/add-local-images.sh
+++ infra/kubernetes/add-local-images.sh
@@ -1,7 +1,11 @@
 # Rebuild, save, and deploy
-docker build -t bardiyabasafa/carsties-auction-api . && \
-docker save bardiyabasafa/carsties-auction-api -o carsties-api.tar && \
-docker cp carsties-api.tar desktop-control-plane:/tmp/ && \
-docker exec desktop-control-plane ctr -n=k8s.io images import /tmp/carsties-api.tar && \
-kubectl rollout restart deployment/carsties-auction-api && \
-rm carsties-api.tar
+docker cp ~/k8s desktop-control-plane:/opt/ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-auction-api.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-bids-api.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-identity-server.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-search-api.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-notification-hub.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-gateway.tar && \ 
+docker exec desktop-control-plane ctr -n=k8s.io images import /opt/k8s/carsties-webapp.tar && \ 
+# kubectl rollout restart deployment/carsties-auction-api && \
+# rm carsties-api.tar
---
+++ infra/kubernetes/save-docker-images.sh
@@ -0,0 +1,7 @@
+docker save bardiyabasafa/carsties-auction-api -o ~/k8s/carsties-auction-api.tar && \
+docker save bardiyabasafa/carsties-identity-server -o ~/k8s/carsties-identity-server.tar && \
+docker save bardiyabasafa/carsties-search-api -o ~/k8s/carsties-search-api.tar && \
+docker save bardiyabasafa/carsties-bids-api -o ~/k8s/carsties-bids-api.tar && \ 
+docker save bardiyabasafa/carsties-notification-hub -o ~/k8s/carsties-notification-hub.tar && \ 
+docker save bardiyabasafa/carsties-gateway -o ~/k8s/carsties-gateway.tar && \ 
+docker save bardiyabasafa/carsties-webapp -o ~/k8s/carsties-webapp.tar 
